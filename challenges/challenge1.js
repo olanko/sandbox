31fecb42e5f66d310c81e71660e9484971806a59
Olli Korhonen <olli.korhonen@iki.fi>
2015-05-21 20:10:53 +0300

    get method

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -1,2 +1,15 @@
 api = require('throttling-test');
 
+api.get('/oakorshonen')
+    .then(function(results) {
+        console.log('results:', results);
+    })
+    .catch(function(err) {
+        console.error('Error received:', err);
+    });
+
+// api.get('/campaigns').then(function(results) {
+//   console.log('results:', results);
+// }, function(error) {
+//   console.error('Error received:', error);
+// });
