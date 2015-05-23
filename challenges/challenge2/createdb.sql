b685de2fd96e41a6068db1790ca189c86c93a7d1
Olli Korhonen <olli.korhonen@iki.fi>
2015-05-23 20:38:03 +0300

    challenge2

---
+++ challenges/challenge2/createdb.sql
@@ -0,0 +1,2 @@
+CREATE TABLE ad_statistics (ad_id int, date date, impressions bigint, clicks bigint, spent bigint);
+CREATE TABLE ad_actions (ad_id int, date date, action varchar(50), count bigint, value bigint);
